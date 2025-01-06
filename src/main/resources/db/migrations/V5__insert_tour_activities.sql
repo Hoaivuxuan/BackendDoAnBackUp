acfe1cedd47163a14ae2fa762f305d6a274509b6
Vu Xuan Hoai <86643318+Hoaivuxuan@users.noreply.github.com>
2025-01-06 16:23:43 +0700

    update

--- src/main/resources/db/migrations/V5__insert_tour_activities.sql
+++ src/main/resources/db/migrations/V5__insert_tour_activities.sql
@@ -1,6 +1,6 @@
 INSERT INTO tour (name, address, description, attraction_id, active) VALUES
-('Tour Sài Gòn', 'Quận 1, TP.HCM', 'Khám phá vẻ đẹp Sài Gòn về đêm', 1, 1),
-('Tour Hà Nội', 'Hoàn Kiếm, Hà Nội', 'Khám phá phố cổ Hà Nội', 2, 1),
+('Buổi biểu diễn múa rối nước Thăng Long', 'Thang Long Water Puppet Theatre, Đinh Tiên Hoàng, Ly Thai To, Hoàn Kiếm, Hanoi', 'Trải nghiệm này sẽ mang đến cho bạn cơ hội khám phá nghệ thuật múa rối nước xa xưa ở Hà Nội. Bạn sẽ ghé thăm nhà hát múa rối nước Thăng Long mang tính biểu tượng, nơi bạn có thể xem những nghệ sĩ múa rối điêu luyện trình diễn. Vé bao gồm buổi biểu diễn múa rối kéo dài 50 phút sử dụng các nhạc cụ truyền thống và những câu truyện dân gian Việt Nam.', 1, 1),
+('Tour đi bộ tham quan thành phố có hướng dẫn viên', 'Hanoi', 'Trong tour riêng này, bạn sẽ được đi bộ và khám phá một số điểm tham quan nổi bật nhất ở Hà Nội. Trước tiên, bạn sẽ cùng hướng dẫn viên ghé Hồ Hoàn Kiếm, nơi được đông đảo người dân địa phương xem là trung tâm thủ đô. Từ đây, bạn sẽ tham quan các địa điểm nổi bật khác như Lăng Chủ tịch Hồ Chí Minh, Nhà sàn Bác Hồ, Phủ Chủ tịch, Bảo tàng Hồ Chí Minh, Chùa Một Cột, cùng nhiều điểm tham quan nổi tiếng khác. Ngoài ra, bạn sẽ được khám phá hai địa danh được bảo tồn kỹ lưỡng và mang tính biểu tượng là Văn Miếu và Đại học Quốc gia. Xuyên suốt tour, hướng dẫn viên sẽ cung cấp nhiều thông tin về Phố cổ, lịch sử, văn hóa và kiến trúc ở Hà Nội.', 2, 1),
 ('Tour Đà Nẵng', 'Sơn Trà, Đà Nẵng', 'Khám phá bãi biển Mỹ Khê', 3, 1);
 
 -- Thêm dữ liệu vào bảng tour_schedule
@@ -11,9 +11,12 @@ INSERT INTO tour_schedule (start_time, end_time, tour_id) VALUES
 
 -- Thêm dữ liệu vào bảng tour_image
 INSERT INTO tour_image (image_url, tour_id) VALUES
-('https://example.com/saigon1.jpg', 1),
-('https://example.com/hanoi1.jpg', 2),
-('https://example.com/danang1.jpg', 3);
+('https://q-xx.bstatic.com/xdata/images/xphoto/max1200/153554304.jpg?k=c4f7d09fa35799ed21178297bd8c6c0bf349d0002eefaec21aff476e3b8012f3&o=', 1),
+('https://r-xx.bstatic.com/xdata/images/xphoto/max1200/153554370.jpg?k=60f836cd315f27d044b5962e15bc519d726bc0bcf0bead6b3b737cb1933165f5&o=', 1),
+('https://q-xx.bstatic.com/xdata/images/xphoto/max1200/153554432.jpg?k=109cb97e143d42661d9be34ced8cacc55152646089a4b2d542cf46e59547af82&o=', 1),
+('https://r-xx.bstatic.com/xdata/images/xphoto/max1200/222171388.jpg?k=5e8c66b47d003a3652ad270a965c8cf854b816db44e51c99ba83e785fd3905c0&o=', 2),
+('https://q-xx.bstatic.com/xdata/images/xphoto/max1200/222171393.jpg?k=5596c5ed79c2225bb8a6868456c37a5cbfa2306b90298d0f397e831bf6496334&o=', 2),
+('https://r-xx.bstatic.com/xdata/images/xphoto/max1200/222171401.jpg?k=eedb91386e8df29830c0783bd55c6cbcefde29a3b8e38e724a94961b42a9cc6f&o=', 2);
 
 -- Thêm dữ liệu vào bảng ticket_class
 INSERT INTO ticket_class (name, price, max_amount, description, tour_id, active) VALUES
